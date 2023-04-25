215bf2b07db799994003f8a6e0a52808879bb16d
ajuvercr <arthur.vercruysse@ugent.be>
2023-04-25 11:08:53 +0200

    add navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import TopAppBar, {
+    AutoAdjust,
+    Row,
+    Section,
+    Title,
+  } from "@smui/top-app-bar";
+  import Button, { Label } from "@smui/button";
+  import "./root.css";
+
+  let topAppBar: TopAppBar;
+</script>
+
+<svelte:head>
+  <title>Connector Architecture</title>
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+  />
+  <link
+    rel="stylesheet"
+    href="/smui.css"
+    media="(prefers-color-scheme: light)"
+  />
+  <link
+    rel="stylesheet"
+    href="/smui-dark.css"
+    media="screen and (prefers-color-scheme: dark)"
+  />
+</svelte:head>
+
+<TopAppBar bind:this={topAppBar} variant="standard">
+  <Row>
+    <Section>
+      <Title on:click={() => goto("/")} style="cursor: pointer;">Connector Architecture</Title>
+    </Section>
+    <Section align="start" toolbar>
+      <Button on:click={() => goto("/docs")}><Label>Docs</Label></Button>
+      <Button on:click={() => goto("/about")}><Label>About</Label></Button>
+    </Section>
+  </Row>
+</TopAppBar>
+
+<AutoAdjust {topAppBar}>
+  <slot />
+</AutoAdjust>
